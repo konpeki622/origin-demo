abf06fb5bad4adf908aed1a7dc281b5315a18850
konpeki622 <512054675@qq.com>
2021-09-08 17:34:44 +0800

    feat: import unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import assetsPlugin from '@originjs/vite-plugin-assets'
 import pagesPlugin from 'vite-plugin-pages'
 import contentPlugin from '@originjs/vite-plugin-content'
-// import Components from 'unplugin-vue-components/vite'
+import Components from 'unplugin-vue-components/vite.d.ts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,14 +13,11 @@ export default defineConfig({
     pagesPlugin(), 
     contentPlugin(),
     // https://github.com/antfu/unplugin-vue-components
-    // Components({
-    //   // allow auto load markdown components under `./src/components/`
-    //   extensions: ['vue', 'md'],
-
-    //   dts: true,
-
-    //   // allow auto import and register components used in markdown
-    //   include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-    // })
+    Components({
+      // allow typescript
+      dts: true,
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+    })
   ]
 })
